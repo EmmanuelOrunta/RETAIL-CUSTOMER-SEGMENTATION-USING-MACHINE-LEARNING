a50e4b2c3d5d0a45948e25ccdcfbfc658ce6bfa6
ORUNTA EMMANUEL UCHENNA <142526376+EmmanuelOrunta@users.noreply.github.com>
2026-04-09 23:09:24 +1000

    Update untitled27.py

--- untitled27.py
+++ untitled27.py
@@ -13,3 +13,15 @@ from sklearn.cluster import KMeans
 df = pd.read_csv("online_retail.csv", encoding='ISO-8859-1')
 # Dataset 2: Australian Retail (Context dataset)
 aus_df = pd.read_csv("australian_retail.csv")
+
+# ================================
+# STEP 3: CLEAN ONLINE RETAIL DATA
+# ================================
+df = df.dropna(subset=['CustomerID'])  # Remove missing customers
+df = df[df['Quantity'] > 0]            # Remove returns/negative sales
+
+# Convert date
+df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
+
+# Create revenue column
+df['TotalPrice'] = df['Quantity'] * df['UnitPrice']
