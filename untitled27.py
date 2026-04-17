2bbbfab47b34496fd0d4c4bb0359e0e66885bc49
ORUNTA EMMANUEL UCHENNA <142526376+EmmanuelOrunta@users.noreply.github.com>
2026-04-17 16:28:08 +1000

    Add simulated regions for Australian customer context
    Added a simulated 'Region' list for customers to align with Australian context.

--- untitled27.py
+++ untitled27.py
@@ -103,7 +103,6 @@ if 'Sales' in aus_df.columns:
     plt.ylabel("Sales")
     plt.show()
 
-# ================================
 # STEP 11: MERGE INSIGHTS (SIMULATION)
 # ================================
 # Add a simulated "Region" to customers to align with Australian context
